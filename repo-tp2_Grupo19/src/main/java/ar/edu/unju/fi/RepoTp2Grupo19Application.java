fc43fad4e01e926a425f6255fd9e4be1f303c0ae
Tapia Natalia <Tapia Natalia@DESKTOP-7Q80SBM>
2023-04-20 17:13:11 -0300

    proyecto tp2 inicial

---
+++ repo-tp2_Grupo19/src/main/java/ar/edu/unju/fi/RepoTp2Grupo19Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoTp2Grupo19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoTp2Grupo19Application.class, args);
+	}
+
+}
